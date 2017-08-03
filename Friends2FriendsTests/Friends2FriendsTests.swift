4be5fe575ab5b4afdd4fa2dc324f5c0861d9bb9d
mbp13i5 <mbp13i5@macbook-pro.local>
2017-08-03 11:56:53 +0530

    Initial Commit

---
+++ Friends2FriendsTests/Friends2FriendsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Friends2FriendsTests.swift
+//  Friends2FriendsTests
+//
+//  Created by mbp13i5 on 03/08/17.
+//  Copyright © 2017 GoHash. All rights reserved.
+//
+
+import XCTest
+@testable import Friends2Friends
+
+class Friends2FriendsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
